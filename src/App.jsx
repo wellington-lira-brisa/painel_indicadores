4e4dc8580724caaf59e3c36e6db81dbe09301e80
wellington-lira-brisa <wellington.lira@grupobrisanet.com.br>
2026-07-10 09:26:32 -0300

    Configura base path para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function CarregandoRota() {
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/login" element={<PaginaLogin />} />
 
